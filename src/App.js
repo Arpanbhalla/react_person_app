46482fbe57e43fda9381040a6138b1f8520822b7
Arpan Bhalla <arpanbhalla@gmail.com>
2018-11-08 12:43:02 +1100

    fixed css configuration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -45,13 +45,6 @@ class App extends Component {
   }
 
   render () {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
 
     let persons = null;
 
@@ -68,26 +61,22 @@ class App extends Component {
           })}
         </div>
       );
-
-      style.backgroundColor = 'red'
-
     }
 
-    const classes = []
+    const assignedClasses = []
     if (this.state.persons.length <= 2) {
-        classes.push('red')
+        assignedClasses.push('red')
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push('bold')
+        assignedClasses.push('bold')
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button
-          style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
